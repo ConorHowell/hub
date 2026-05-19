0321304c8720b7d16fc850ceaf16a9fcbfb48044
Conor Howell <conorbhowell@gmail.com>
2026-05-18 22:14:28 -0700

    Add Morrowind Quest Helper to hub

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 set -e
-SERVER_USER=bitnami
-SERVER_IP=52.13.231.223
-SSH_KEY=~/.ssh/LightsailDefaultKey-us-west-2.pem
+source "$(dirname "$0")/../server.conf"
 REMOTE_PATH=/opt/bitnami/wordpress/hub/
+chmod 600 "$SSH_KEY" 2>/dev/null || true
 
 echo "Deploying Hub to $SERVER_IP:$REMOTE_PATH ..."
 scp -i "$SSH_KEY" index.html "$SERVER_USER@$SERVER_IP:$REMOTE_PATH"
